a9b1c0e599cfc9147264defb59043aa422819db2
khuseyn-khasiev <khuseyn-khasiev@mail.ru>
2023-04-16 00:09:27 +0300

    добавить логирование запросов и ошибок

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes/index');
 const { errorHandler } = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -13,7 +14,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/moviesdb', {
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(requestLogger); // подключаем логгер запросов
 app.use(routes);
+app.use(errorLogger); // подключаем логгер ошибок
 
 app.use(errorHandler); // централизованный обработчик ошибок
 app.listen(PORT, () => {

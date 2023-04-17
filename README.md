80d21f96121358f64daab6cffe006b8d6c66e46c
khuseyn-khasiev <khuseyn-khasiev@mail.ru>
2023-04-17 10:40:35 +0300

    добавить ссылку на сервер в readme.md, добавить cors и создать отдельный модуль middleware, установить пакет dotenv для доступа к env файлу

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # movies-explorer-api
+
+# Проект Movies-explorer бэкенд
+
+## Ссылка на репозиторий проекта
+[movies-explorer-api](https://github.com/khuseynkhasiev/movies-explorer-api)
+
+## Домен по которому можно обратиться к серверу
+[https://api.husmovies.nomoredomains.monster](https://api.husmovies.nomoredomains.monster)
+
+## Директории
+
+`/routes` — папка с файлами роутера  
+`/controllers` — папка с файлами контроллеров пользователя и карточки   
+`/models` — папка с файлами описания схем пользователя и карточки
+
+Остальные директории вспомогательные, создаются при необходимости разработчиком
+
+## Запуск проекта
+
+`npm run start` — запускает сервер   
+`npm run dev` — запускает сервер с hot-reload
--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const helmet = require('helmet');
 const routes = require('./routes/index');
 const { errorHandler } = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const limiter = require('./middlewares/limiter');
+const { options } = require('./middlewares/handleOptions');
 
 const { PORT = 3001, mongoDb = 'mongodb://127.0.0.1:27017/moviesdb' } = process.env;
 
 const app = express();
+app.use('*', cors(options));
 app.use(limiter);
 mongoose.connect(mongoDb, {
   useNewUrlParser: true,
